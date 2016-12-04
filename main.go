cbd2c0f92402c767a9d4af777e16a302e56117c0
alvarpq <alvarogudis@gmail.com>
2016-12-04 16:57:55 -0500

    pushing things towards proper project specs

--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
+/*
+*alvarpq 12/4/2016
+ */
+
 import(
 	"fmt"
 	"strconv"
 	"math/rand"
 	"os"
-	"time"
 )
 
-//input params:1 max number of sequences,2 length of sequence, 3number of motifs in, 4max length of motif,5number of possible changes to a motif
+//input params:1 max number of sequences,2 length of sequence, 3number of motifs in, 4max length of motif,5number of possible changes to a motif,6activeSubRegion
 func main() {
+	fmt.Print("oWo\n")
+
+
 	max,_:=strconv.Atoi(os.Args[4])
 	length,_:=strconv.Atoi(os.Args[3])
 	var motifs []string
@@ -21,45 +27,44 @@ func main() {
 	max,_ = strconv.Atoi(os.Args[1])
 
 	for x:=0;x<max;x++{
-		seq,_ :=strconv.Atoi(os.Args[2])
-		go sequenceGenome(changes, seq, motifs)
+		seq,_ :=strconv.Atoi(os.Args[6])
+		sequenceGenome(changes, seq, motifs)
 	}
-	time.Sleep(99999999)
-
 }
 
 func sequenceGenome(changeNum int, maxLength int, motifs []string){
-	fmt.Print("oWo\n")
 	max,_:=strconv.Atoi(os.Args[4])
-	sequence:=make(chan string)
-
+	machC,_:=strconv.Atoi(os.Args[2])
 	shortestMotif := max
 	for x:=0;x<len(motifs);x++{
 		if len(motifs[x]) < shortestMotif{
 			shortestMotif = len(motifs[x])
 		}
 	}
-	genome :=""
-	for x:=0;x<100;x++{
-		jam := improvise(changeNum, motifs[rand.Intn(len(motifs))])
-		random := generateRandom(rand.Intn(max))
+	genome := generateRandom(rand.Intn(machC/2))
+	gStart := len(genome)
+	motC := make(chan string)
+	randC := make(chan string)
+
+	motifNums := make(chan int)
+	go getMotifs(motifs,changeNum,motC, motifNums)
+
+	go getRandomStrings(maxLength,randC)
+
+	for ;len(genome)<maxLength + gStart;{
 		select {
-		case sequence <-jam:
-			fmt.Println(jam)
-		case sequence <-random:
-			fmt.Println(random)
-
-		default:
-			fmt.Println(jam + "D:")
-
+		case sequence:= <-motC:
+			cardinal:= <- motifNums
+			printer :="> m"  +strconv.Itoa(cardinal) +  " "+ strconv.Itoa(len(genome)) + ", " + strconv.Itoa(len(genome) + len(sequence) - 1)+ ": " + sequence[2:]
+			println(printer)
+			genome += sequence[2:]
+		case sequence:= <-randC:
+			genome += sequence
 
 		}
-		fmt.Println(x)
-		latestStr:=<-sequence
-		close(sequence)
-
-		genome += latestStr
 	}
+
+	genome += generateRandom(machC - len(genome))
 	fmt.Println(genome + ":(")
 
 }
@@ -114,4 +119,23 @@ func getNucleotide() (string) {
 		return "d"
 	}
 	
+}
+
+func getMotifs(motifs []string,changes int, motifChannel chan <- string, motifNums chan <- int){
+	for{
+		motif := rand.Intn(len(motifs))
+		if(motif < 10){
+			motifChannel <- "0" + strconv.Itoa(motif) + improvise(changes, motifs[motif])
+
+		}else {
+			motifChannel <- strconv.Itoa(motif) + improvise(changes, motifs[motif])
+		}
+		motifNums <- motif
+	}
+}
+
+func getRandomStrings(maxLength int, randomChannel chan <- string){
+	for{
+		randomChannel <-generateRandom(rand.Intn(maxLength))
+	}
 }
